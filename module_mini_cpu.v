db76446b5c02f9a39de43be3d86f56a4b699aad1
--global <--global>
2025-03-29 10:33:15 -0300

    eu achei cpu

--- module_mini_cpu.v
+++ module_mini_cpu.v
@@ -1,38 +1,42 @@
 module module_mini_cpu(
 	input [17:0] switch,
-	input b_en, b_send, clk
+	input b_en, b_send, clk,
+	output EN, RW, RS
 );
-
-	paramenter IDLE = 0, GET = 1, SET = 2, RESET = 3;
+	
+	parameter IDLE = 0, GET = 1, SET = 2, RESET = 3;
 
 	parameter OFF = 0, WAIT_PRESS_EN = 1,
 				WAIT_UNPRESS_EN = 2,
-				GET1 = 3, GET1_OK = 4, GET2 = 5, GET2_OK = 6,  RESET = 7, RESET_OK = 8, CALCULATE = 9, ULA_OK = 10,
-				SET = 11, SET_OK = 12, GET_UPDATE = 13, GET_UP = 13, SHOW = 15;
+				GET1 = 3, GET1_OK = 4, GET2 = 5, GET2_OK = 6,  RESET_RAM = 7, RESET_OK = 8, CALCULATE = 9, ULA_OK = 10,
+				SET_RAM = 11, SET_OK = 12, GET_UPDATE = 13, GET_UP = 13, SHOW_DISPLAY = 15;
 	
 	parameter LOAD = 0, ADD = 1, 
 				ADDI = 2, SUB = 3, 
 				SUBI = 4, MUL = 5, 
 				CLEAR = 6, DISPLAY = 7;
 				
+	parameter DISPLAY_OFF = 1, SHOW = 2;
+	
 	reg on = 0;
-	
-	reg [15:0] value1 = 0;
-	reg [15:0] value2 = 0;
-	wire [15:0 op_result = 0;
-	reg [15:0] value3 = 0;
-	reg [2:0] operation = 0;
-
 	reg [3:0] state = OFF;
-	
-	reg [15:0] show_value = 0;
-	reg [1:0] mode_ram;
+	reg [1:0] mode_ram = 0;
+	reg [2:0] operation = 0;
 	reg [3:0] address;
 	reg [15:0] data_in;
+	reg [15:0] value1 = 0;
+	reg [15:0] value2 = 0;
+	reg [15:0] value3 = 0;
+	reg [15:0] show_value = 0;
+	wire done_ram;
+	wire done_ula;
+	wire done_display;
+	reg [1:0] op_display = 0;
+	reg [2:0] type_op;
 	wire [15:0] data_out;
-	wire done_ram = 0;
+	wire [15:0] op_result;
+	wire wire_en, wire_rw, wire_rs;
 	
-	wire done_ula = 0;
 	
 	RAM ram (
 		.clk(clk);
@@ -52,31 +56,44 @@ module module_mini_cpu(
 		.done(done_ula);
 	);
 	
+	DISPLAY display (
+		.clk(clk);
+		.operation(op_display);
+		.opcode(type_op);
+		.addr(address);
+		.data_addr(show_value);
+		.EN(wire_en);
+		.RW(wire_rw);
+		.RS(wite_rs);
+		.done(done_display);
+	);
+	
+	
 	always @(posedge b_en) begin
 		on <= ~on;
 	end
 	
 	always @(posedge clk) begin
 		case (state)
-			OFF: begin state <= (on)? WAIT_PRESS_EN : state; end
-			WAIT_PRESS_EN: begin state <= (on)? (~b_send)? WAIT_UNPRESS_EN : state : RESET; end
+			OFF: begin state <= (done_display)? (on)? WAIT_PRESS_EN : state : state; end
+			WAIT_PRESS_EN: begin state <= (on)? (~b_send)? WAIT_UNPRESS_EN : state : RESET_RAM; end
 			WAIT_UNPRESS_EN: begin
 				if (on) begin
 					if (~b_send) begin
 						case (switch[17:15])
-							LOAD: state <= SET;
-							CLEAR: state <= RESET;
-							DISPLAY: state <= SHOW;
+							LOAD: state <= SET_RAM;
+							CLEAR: state <= RESET_RAM;
+							DISPLAY: state <= SHOW_DISPLAY;
 							default: state <= GET1;
 						endcase
 					end else begin
 						state <= state;
 					end
 				end else begin
-					state <= RESET;
+					state <= RESET_RAM;
 				end
 			end
-			GET1: begin state <= (on)? (done_ram)? GET1_OK : state : RESET; end
+			GET1: begin state <= (on)? (done_ram)? GET1_OK : state : RESET_RAM; end
 			GET1_OK: begin
 				if (on) begin
 					case(switch[17:15])
@@ -84,30 +101,30 @@ module module_mini_cpu(
 						ADDI: state <= CALCULATE;
 						SUB: state <= GET2;
 						SUBI: state <= CALCULATE;
-						DISPLAY: state <= SHOW;
+						DISPLAY: state <= SHOW_DISPLAY;
 					endcase
 				end else begin
-					state <= RESET;
+					state <= RESET_RAM;
 				end
 			end
-			GET2: begin state <= (on)? (done_ram)? GET2_OK: state : RESET; end
-			GET2_OK: begin state <= (on)? CALCULATE : RESET; end
-			RESET: begin state <= (on)? (done_ram)? RESET_OK : state : OFF; end
-			RESET_OK: begin state <= (on)? SHOW : OFF; end
-;			CALCULATE: begin state <= (on)? (done_ula)? ULA_OK: state : RESET; end
-			ULA_OK: begin state <= (on)? SET : RESET; end
-			SET: begin state <= (on)? (done_ram)? SET_OK : state : RESET; end
-			SET_OK: begin state <= (on)? GET_UPDATE : RESET;
-			GET_UPDATE: begin state <= (on)? (done_ram)? GET_UP_OK : state : RESET end
-			GET_UP_OK: begin state <= (on)? SHOW : RESET; end
-			SHOW: begin state <= (on)? INIT : RESET; end
+			GET2: begin state <= (on)? (done_ram)? GET2_OK: state : RESET_RAM; end
+			GET2_OK: begin state <= (on)? CALCULATE : RESET_RAM; end
+			RESET_RAM: begin state <= (on)? (done_ram)? RESET_OK : state : OFF; end
+			RESET_OK: begin state <= (on)? GET_UPDATE : OFF; end
+;			CALCULATE: begin state <= (on)? (done_ula)? ULA_OK: state : RESET_RAM; end
+			ULA_OK: begin state <= (on)? SET_RAM : RESET_RAM; end
+			SET_RAM: begin state <= (on)? (done_ram)? SET_OK : state : RESET_RAM; end
+			SET_OK: begin state <= (on)? GET_UPDATE : RESET_RAM;
+			GET_UPDATE: begin state <= (on)? (done_ram)? GET_UP_OK : state : RESET_RAM end
+			GET_UP_OK: begin state <= (on)? SHOW_DISPLAY : RESET_RAM; end
+			SHOW_DISPLAY: begin state <= (done_display)? (on)? INIT : RESET_RAM : state; end
 		endcase
 	end
 	
 	always @(posedge clk) begin
 		case (state)
-			OFF: display(OFF);
-			WAIT_PRESS_EN: mode_ram <= IDLE; operation <= 0;
+			OFF: op_display <= DISPLAY_OFF;
+			WAIT_PRESS_EN: begin mode_ram <= IDLE; operation <= 0; op_display <= IDLE;
 			GET1: begin 
 				if (switch[17:15] == DISPLAY) begin
 					mode_ram <= GET;
@@ -126,7 +143,6 @@ module module_mini_cpu(
 					MULT: value2 <= switch[6:0];
 					default: value2 <= 0;
 				endcase
-				value2 <= 0;
 			end
 			GET2: begin
 				mode_ram <= GET;
@@ -136,23 +152,31 @@ module module_mini_cpu(
 				mode_ram <= IDLE;
 				value2 <= data_out;
 			end
-			RESET: mode_ram <= RESET; 
+			RESET_RAM: mode_ram <= RESET;
 			RESET_OK: mode_ram <= IDLE;
 			CALCULATE: operation <= switch[17:15];
 			ULA_OK: begin value3 <= op_result; operation <= 0;
-			SET: begin 
+			SET_RAM: begin 
 				mode_ram <= SET;
 				address <= switch[14:11];
 				data_in <= (switch[17:15] == LOAD)? switch[6:0] : value3;
 			SET_OK: mode_ram = IDLE;
 			GET_UPDATE: begin
 				mode_ram <= GET;
-				address <= switch[17:15];
+				address <= switch[14:11];
 			end
-			GET_UP_OK: begin show_value = data_out; mode_ram = IDLE;
-			SHOW: 
-			
-								
+			GET_UP_OK: begin mode_ram = IDLE;
+			SHOW_DISPLAY: begin 
+				op_display <= SHOW;
+				op_code <= switch[17:15];
+				address < switch[14:11];
+				show_value <= data_out;
+			end
+		endcase
 	end
+	
+	assign EN = wire_en;
+	assign WR = wire_rw;
+	assign RS = wire_rs;
 	
 endmodule
